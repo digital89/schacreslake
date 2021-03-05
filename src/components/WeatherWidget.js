f340a59c825dd2675967253f60231cca294be7a1
Jared Reich <jreich1@godaddy.com>
2021-03-05 15:42:53 -0700

    Add more polish.

--- src/components/WeatherWidget.js
+++ src/components/WeatherWidget.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import dayjs from 'dayjs';
 
 const lat = 52.480818160508875;
 const lon = -113.76193404392085;
@@ -20,53 +21,55 @@ const WeatherWidget = () => {
   }, []);
 
   /*
-clouds: 63
-dew_point: -33.05
-dt: 1612760400
-feels_like: -34.9
-humidity: 76
-pop: 0
-pressure: 1037
-temp: -30.45
-uvi: 0
-visibility: 10000
-weather: [Object] (1)
-wind_deg: 241
-wind_speed: 0.81
+  clouds: 63
+  dew_point: -33.05
+  dt: 1612760400
+  feels_like: -34.9
+  humidity: 76
+  pop: 0
+  pressure: 1037
+  temp: -30.45
+  uvi: 0
+  visibility: 10000
+  weather: [Object] (1)
+  wind_deg: 241
+  wind_speed: 0.81
   */
 
   return (
     <>
       {weatherData ? (
-        <div
-          className="table-container"
-          style={{ maxHeight: "300px", overflowY: "scroll" }}
-        >
+        <div className="table-container">
           <table className="table is-fullwidth">
             <thead>
               <tr>
                 <th>Time</th>
                 <th>°C</th>
-                <th>Feels Like</th>
+                <th>Feels Like (°C)</th>
+                <th>POP</th>
                 <th>Humidity</th>
-                <th>Wind</th>
+                <th>Wind (km/h)</th>
               </tr>
             </thead>
             <tbody>
-              {weatherData.hourly.map((hourly) => (
-                <tr>
-                  <td>{hourly.dt}</td>
-                  <td>{hourly.temp}</td>
-                  <td>{hourly.feels_like}</td>
-                  <td>{hourly.humidity}</td>
-                  <td>{hourly.wind_speed}</td>
-                </tr>
-              ))}
+              {weatherData.hourly.map((hourly) => {
+                const date = new Date(hourly.dt * 1000);
+                return (
+                  <tr>
+                    <td>{dayjs(date.toUTCString()).format('ddd, MMM DD, h:mma')}</td>
+                    <td>{Math.round(hourly.temp)}</td>
+                    <td>{Math.round(hourly.feels_like)}</td>
+                    <td>{`${Math.round(hourly.pop * 100)}%`}</td>
+                    <td>{`${hourly.humidity}%`}</td>
+                    <td>{hourly.wind_speed}</td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
         </div>
       ) : (
-        <div>loading...</div>
+        <div>Loading...</div>
       )}
     </>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import * as React from "react";
 import { InlineWidget } from "react-calendly";
 
 import Layout from "../components/Layout";
-// import CalendarWidget from "../components/CalendarWidget";
 import WeatherWidget from "../components/WeatherWidget";
 
 const IndexPage = () => {
@@ -15,7 +14,7 @@ const IndexPage = () => {
               <div className="container">
                 <div className="navbar-brand">
                   <a className="navbar-item">
-                    <img src="https://placekitten.com/300/100" alt="Logo" />
+                    <img src="/images/icon.png" alt="Logo" />
                   </a>
                 </div>
                 <div className="navbar-menu">
@@ -47,18 +46,20 @@ const IndexPage = () => {
           </div>
         </section>
 
-        <section className="section">
-          <div className="container">
-            <div>about blurb</div>
+        <section className="section section-1">
+          <div className="container content">
+            <h4>
+              MY DREAM COMING TRUE. 20,000 m3 to go. I've had this dream for a lot of years to build a waterski lake, and the only thing better... now it's starting to come true. Check back to see how this crazy adventure is going.
+            </h4>
           </div>
         </section>
 
-        <section className="section">
-          <div className="container">
+        <section className="section section-1">
+          <div className="container content">
             <div>
-              {/* <CalendarWidget /> */}
+              <h2>Book a time slot:</h2>
               <InlineWidget
-                url="https://calendly.com/jaredreich"
+                url="https://calendly.com/digital89"
                 styles={{
                   height: "630px",
                   // maxWidth: "100px",
@@ -69,9 +70,32 @@ const IndexPage = () => {
           </div>
         </section>
 
-        <section className="section section-inverted">
-          <div className="container">
+        <section className="section">
+          <div className="container content">
+            <h2>By-the-hour weather:</h2>
             <WeatherWidget />
+          </div>
+        </section>
+
+        <section className="section">
+          <div className="container content">
+            <h2>Location:</h2>
+            <iframe
+              allowFullScreen
+              frameBorder="0"
+              loading="lazy"
+              marginHeight="0"
+              marginWidth="0"
+              scrolling="no"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2429.91321205774!2d-113.76414418387218!3d52.4807070798068!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x53750426db5f53b1%3A0x959a09c11b0842c7!2sSchacres%20Lake!5e0!3m2!1sen!2sca!4v1614983733182!5m2!1sen!2sca"
+              style={{
+                border: 'none',
+                height: '300px',
+                width: '100%',
+              }}
+              title="Google Maps View"
+            />
+
           </div>
         </section>
 
