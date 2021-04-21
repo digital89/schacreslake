23d7225b49d115e943dacea1e0f75b76dc546825
Jared Reich <jaredreich@gmail.com>
2021-04-21 09:09:41 -0600

    Re-work manifest stuff.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -24,8 +24,20 @@ const Layout = ({ children }) => {
 
         <link rel="canonical" href={`https://${domainName}`} />
 
-        <link rel="apple-touch-icon" sizes="180x180" href="/images/icon-180-180.png" />
+        {/* <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png"></link>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32-32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16-16.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#d3d3d3" />
+        <meta name="msapplication-TileColor" content="#d3d3d3" /> */}
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#D3D3D3" />
+        <meta name="theme-color" content="#D3D3D3" />
       </Helmet>
 
       {children}
