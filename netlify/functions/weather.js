9e9b0563006580d9117541e4379105e02828f092
Jared Reich <jaredreich@gmail.com>
2022-07-15 22:52:49 -0600

    Add more setup to handle CORS.

--- netlify/functions/weather.js
+++ netlify/functions/weather.js
@@ -1,15 +1,34 @@
 const axios = require('axios');
 const xml2json = require('xml2json');
 
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
+}
+
 exports.handler = async function(event, context) {
   const { data } = await axios.get('https://dd.weather.gc.ca/citypage_weather/xml/AB/s0000297_e.xml');
   const json = xml2json.toJson(data);
 
+  switch (event.httpMethod) {
+    case 'OPTIONS':
+      // To enable CORS...
+      return {
+        body: 'This was a preflight call!',
+        headers,
+        statusCode: 200, // Must be 200 otherwise pre-flight call fails
+      };
+    case 'GET':
+      return {
+        body: json,
+        headers,
+        statusCode: 200,
+      };
+  }
+
   return {
-      body: json,
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-      },
+    statusCode: 500,
+    body: 'Unrecognized HTTP Method, must be one of GET/POST/PUT/DELETE/OPTIONS.'
   };
 }
